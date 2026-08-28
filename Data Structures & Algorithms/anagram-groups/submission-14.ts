ccd30dcff86f2231b9c2de6812610d4abdd4f008
tmbowman26-design <tmbowman26@gmail.com>
2026-08-28 11:44:23 -0500

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const map = new Map<string, string[]>();
+
+        for (const str of strs) {
+            const count = new Array(26).fill(0);
+            for (const char of str) {
+                count[char.charCodeAt(0) - 97]++;
+            }
+            const key = count.join(',');
+            if(!map.has(key)) {
+                map.set(key, []);
+            }
+            map.get(key)!.push(str)
+        }
+
+        return [...map.values()];
+    }
+}
