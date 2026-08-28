1330a5e803160fe71411ae49a0c848c4c2b71fa6
tmbowman26-design <tmbowman26@gmail.com>
2026-08-28 14:39:47 -0500

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const result = new Array(nums.length).fill(1);
+
+        let prefix = 1;
+        let suffix = 1;
+
+        for (let i = 0; i < nums.length; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        for (let i = nums.length - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return result;
+    }
+}
