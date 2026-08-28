c25775adf6b209ff3e59b7c49c80de5fd45a2743
tmbowman26-design <tmbowman26@gmail.com>
2026-08-28 15:31:43 -0500

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let left = 0;
+        let right = s.length - 1;
+        while (left < right) {
+            const leftChar = s[left].toLowerCase();
+            const rightChar = s[right].toLowerCase();
+
+            if (!isAlphanumeric(leftChar)) {
+                left++; continue;
+            }
+            if (!isAlphanumeric(rightChar)) {
+                right--; continue;
+            }
+            if (leftChar !== rightChar) {
+                return false;
+            }
+            left++; right--;
+        }
+
+        return true;
+    }
+}
+
+function isAlphanumeric(s: string) {
+    const char = s.charCodeAt(0);
+    return (char >= 97 && char <= 122) || (char >= 48 && char <= 57);
+}
