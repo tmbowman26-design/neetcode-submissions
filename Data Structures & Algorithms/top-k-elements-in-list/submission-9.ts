0d3204c016dd53718185713a5f9af05b999d91ed
tmbowman26-design <tmbowman26@gmail.com>
2026-08-28 12:48:49 -0500

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const map = new Map<number, number>();
+
+        for (const num of nums) {
+            map.set(num, (map.get(num) ?? 0) + 1);
+        }
+
+        const buckets = new Array(nums.length + 1).fill(null).map(() => []);
+
+        for (const [num, count] of map) {
+            buckets[count].push(num);
+        }
+
+        const result: number[] = [];
+
+        for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+            for (const num of buckets[i]) {
+                result.push(num);
+                if (result.length === k) break;
+            }
+        }
+        return result;
+    }
+}
