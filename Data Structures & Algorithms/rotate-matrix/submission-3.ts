de9a81b66f8a6a4bc341a8df0dbdee8babc20099
tmbowman26-design <tmbowman26@gmail.com>
2026-08-29 11:48:25 -0500

    Add: rotate-matrix - submission-3

---
+++ Data Structures & Algorithms/rotate-matrix/submission-3.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(matrix: number[][]): void {
+        matrix.reverse();
+
+        for (let row = 0; row < matrix.length; row++) {
+            for (let column = row; column < matrix[row].length; column++) {
+                [matrix[row][column], matrix[column][row]] = 
+                [matrix[column][row], matrix[row][column]];
+            }
+        }
+    }
+}
