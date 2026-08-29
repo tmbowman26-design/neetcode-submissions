1c07a1aca9e03d580a12bdb93a3ae6f6907e3e7e
tmbowman26-design <tmbowman26@gmail.com>
2026-08-29 17:03:16 -0500

    Add: set-zeroes-in-matrix - submission-2

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-2.ts
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    setZeroes(matrix: number[][]): void {
+        const rows = matrix.length;
+        const columns = matrix[0].length;
+        let firstRowZero = false;
+
+        for (let row = 0; row < rows; row++) {
+            for (let column = 0; column < columns; column++) {
+                if (matrix[row][column] === 0) {
+                    matrix[0][column] = 0;
+                    if (row > 0) {
+                        matrix[row][0] = 0;
+                    } else {
+                        firstRowZero = true;
+                    }
+                }
+                
+            }
+        }
+        for (let row = 1; row < rows; row++) {
+            for (let column = 1; column < columns; column++) {
+                if (matrix[0][column] === 0 || matrix[row][0] === 0) {
+                    matrix[row][column] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] === 0) {
+            for (let row = 0; row < rows; row++) {
+                matrix[row][0] = 0;
+            }
+        }
+
+        if(firstRowZero) {
+            for (let column = 0; column < columns; column++) {
+                matrix[0][column] = 0;
+            }
+        }
+    }
+}
