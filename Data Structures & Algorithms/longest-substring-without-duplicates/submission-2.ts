9e95c5c5a85017fc629dd28a93ab6bb7ee5f4663
tmbowman26-design <tmbowman26@gmail.com>
2026-08-28 21:09:50 -0500

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        const seen = new Set<string>();
+        let left = 0;
+        let maxLength = 0;
+
+        for(let right = 0; right < s.length; right++) {
+            while(seen.has(s[right])) {
+                seen.delete(s[left]);
+                left++;
+            }
+            seen.add(s[right]);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
