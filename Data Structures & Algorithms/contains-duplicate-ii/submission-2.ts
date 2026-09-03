7013658f37b8b2a5c1423d24154ecbaa41432c98
tmbowman26-design <tmbowman26@gmail.com>
2026-09-03 12:27:02 -0500

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums: number[], k: number): boolean {
+        const set = new Set<number>();
+        let left = 0;
+        for (let right = 0; right < nums.length; right++) {
+            if (right - left > k) {
+                set.delete(nums[left]);
+                left++;
+            }
+            if (set.has(nums[right])) return true;
+            set.add(nums[right]);
+        }
+        return false;
+    }
+}
