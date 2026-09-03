2cb12f9e26b072d67440a4acc8adc436a794663c
tmbowman26-design <tmbowman26@gmail.com>
2026-09-03 11:17:52 -0500

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minPrice = prices[0];
+        let maxProfit = 0;
+        for (let i = 1; i < prices.length; i++) {
+            const profit = prices[i] - minPrice;
+            maxProfit = Math.max(profit, maxProfit);
+            minPrice = Math.min(prices[i], minPrice);
+        }
+        return maxProfit;
+    }
+}
