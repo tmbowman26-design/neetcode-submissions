ee56fd40b92915ed681022621b28527138b6317d
tmbowman26-design <tmbowman26@gmail.com>
2026-09-04 10:32:58 -0500

    Add: spiral-matrix - submission-13

---
+++ Data Structures & Algorithms/spiral-matrix/submission-13.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix: number[][]): number[] {
+        let top = 0;
+        let right = matrix[0].length - 1;
+        let bottom = matrix.length - 1;
+        let left = 0;
+        const result: number[] = [];
+        while (top <= bottom && left <= right) {
+            for (let col = left; col <= right; col++) {
+                result.push(matrix[top][col]);
+            }
+            top++;
+            for (let row = top; row <= bottom; row++) {
+                result.push(matrix[row][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (let col = right; col >= left; col--) {
+                    result.push(matrix[bottom][col]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (let row = bottom; row >= top; row--) {
+                    result.push(matrix[row][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
