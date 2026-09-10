3942f23290b4d7e6cf7feff32f34925df0934e61
tmbowman26-design <tmbowman26@gmail.com>
2026-09-10 16:41:19 -0500

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.ts
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let encodedStr: string = '';
+        for (const str of strs) {
+            encodedStr += str.length + '#' + str;
+            // encodedStr += `${str.length}#${str}`;
+        }
+        return encodedStr;
+    }
+
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        let result: string[] = [];
+        let i: number = 0;
+        while (i < str.length) {
+            let j: number = i;
+
+            while (str[j] !== '#') {
+                j++;
+            }
+            const length: number = Number(str.slice(i, j));
+
+            const word: string = str.slice(j + 1, j + 1 + length);
+
+            result.push(word);
+
+            i = j + 1 + length;
+        }
+        return result;
+    }
+}
