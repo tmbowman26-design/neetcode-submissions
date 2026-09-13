d82da5584edb1cfce2a64b3409edc658b707384f
tmbowman26-design <tmbowman26@gmail.com>
2026-09-13 15:49:57 -0500

    Add: duplicate-integer - submission-19

---
+++ Data Structures & Algorithms/duplicate-integer/submission-19.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const set = new Set<number>();
+        for (const num of nums) {
+            if (set.has(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
