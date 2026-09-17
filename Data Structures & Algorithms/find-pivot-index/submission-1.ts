b581ecf7e6658fdce83325d359ba4c9467a832ac
tmbowman26-design <tmbowman26@gmail.com>
2026-09-17 11:55:06 -0500

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    pivotIndex(nums: number[]): number {
+        let total: number = 0;
+        let leftSum: number = 0;
+        for (let i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        for (let i = 0; i < nums.length; i++) {
+            const rightSum = total - leftSum - nums[i];
+
+            if (leftSum === rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
