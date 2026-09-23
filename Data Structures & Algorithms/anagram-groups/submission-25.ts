7af353cb349939979716b75c9841f402ed100639
tmbowman26-design <tmbowman26@gmail.com>
2026-09-23 16:46:43 -0500

    Add: anagram-groups - submission-25

---
+++ Data Structures & Algorithms/anagram-groups/submission-25.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const anagrams = new Map<string, string[]>();
+        for (const str of strs) {
+            const anchor = str.split('').sort().join('');
+            if (anagrams.has(anchor)) {
+                anagrams.get(anchor)!.push(str);
+            } else {
+                anagrams.set(anchor, [str])
+            }
+        }
+        return [...anagrams.values()]
+    }
+}
