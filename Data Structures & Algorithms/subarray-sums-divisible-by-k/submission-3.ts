f47ae3d219b8549e010105fbcc0e041ef25590ca
tmbowman26-design <tmbowman26@gmail.com>
2026-09-25 12:22:00 -0500

    Add: subarray-sums-divisible-by-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sums-divisible-by-k/submission-3.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraysDivByK(nums: number[], k: number): number {
+        const map = new Map<number, number>();
+        map.set(0, 1);
+        let runningSum = 0, count = 0;
+        for (const num of nums) {
+            runningSum += num;
+            const remainder = ((runningSum % k) + k) % k;
+            if (map.has(remainder)) {
+                count += map.get(remainder);
+                map.set(remainder, map.get(remainder) + 1);
+            } else {
+                map.set(remainder, 1);
+            }
+        }
+        return count;
+    }
+}
